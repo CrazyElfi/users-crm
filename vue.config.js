f357a1fc89849c51c84a7f973e91f17b6c3a1bbe
elfi <alrigaff@gmail.com>
2021-04-08 18:18:22 +0300

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // options...
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/'
+        : '/',
+    assetsDir: '',
+    productionSourceMap: false,
+    filenameHashing: true,
+}
